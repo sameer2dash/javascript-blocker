f2c9141c9a0a625b719052d8b6fa1f703e0285f6
sameer2dash <dash.sameer@gmail.com>
2021-04-17 23:35:18 -0700

    First commit

---
+++ options.js
@@ -0,0 +1,25 @@
+// localize the HTML
+document.querySelectorAll('[data-message]').forEach(elem => {
+    elem.textContent = chrome.i18n.getMessage(elem.dataset.message);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    chrome.storage.sync.get({
+        refresh: true
+    }, (items) => {
+        document.getElementById('refresh').checked = items.refresh;
+    });
+});
+
+document.getElementById('save').addEventListener('click', () => {
+    const refresh = document.getElementById('refresh').checked;
+    chrome.storage.sync.set({
+        refresh: refresh
+    }, () => {
+        const message = document.getElementById('message');
+        message.textContent = 'Options saved.';
+        setTimeout(() => {
+            message.textContent = '\u00A0';
+        }, 1000);
+    });
+});
---
+++ popup.js
@@ -0,0 +1,59 @@
+const message = document.getElementById('message');
+
+// localize the HTML
+document.querySelectorAll('[data-message]').forEach(elem => {
+    elem.textContent = chrome.i18n.getMessage(elem.dataset.message);
+});
+
+document.getElementById('optionsLink').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (chrome.runtime.openOptionsPage) {
+        chrome.runtime.openOptionsPage();
+    } else {
+        window.open(chrome.runtime.getURL('options.html'));
+    }
+});
+
+
+async function executeToggleJs() {
+    message.textContent = chrome.i18n.getMessage('workingText');
+    const tabs = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+
+    if (tabs[0] && tabs[0].url && chrome.contentSettings.javascript) {
+        const url = new URL(tabs[0].url);
+        const pattern = url.origin + '/*';
+        if (!pattern.includes('http')) {
+            message.textContent = chrome.i18n.getMessage('invalidSite');
+            return;
+        }
+        const isIncognito = tabs[0].incognito;
+
+        chrome.contentSettings.javascript.get({
+            primaryUrl: pattern,
+            incognito: isIncognito
+        }, (details) => {
+            if (typeof details === 'undefined') {
+                message.textContent = chrome.i18n.getMessage('noJs');
+                return;
+            }
+            let newSetting = 'block';
+            if (details.setting === 'block') {
+                newSetting = 'allow';
+            }
+
+            chrome.contentSettings.javascript.set({
+                primaryPattern: pattern,
+                scope: isIncognito ? 'incognito_session_only' : 'regular',
+                setting: newSetting
+            }, () => {
+                message.textContent = chrome.i18n.getMessage('js_' + newSetting + 'ed');
+                // setTimeout(() => window.close(), 2000);
+                chrome.storage.sync.get({ refresh: true }, (items) => {
+                    items.refresh && chrome.tabs.reload(tabs[0].id || undefined);
+                });
+            });
+        });
+    }
+}
+
+executeToggleJs();
